aab2380fd4fe847dc352952839471b0e337e1308
jpmoraess <moraess.it@gmail.com>
2024-03-20 10:09:02 -0300

    ajustes

---
+++ order-service/src/main/java/br/com/moraesit/order/service/config/VirtualThreadConfig.java
@@ -0,0 +1,18 @@
+package br.com.moraesit.order.service.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+public class VirtualThreadConfig {
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
+}
